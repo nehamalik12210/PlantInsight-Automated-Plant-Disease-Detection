824cf67e0dc08d2b0bbb3950a0f106c87a123e06
neha malik <134438830+nehamalik12210@users.noreply.github.com>
2026-03-07 03:52:42 +0530

    Delete prev Folder and add to new one

---
+++ Web_Application/Static/js/script.js
@@ -0,0 +1,18 @@
+document.getElementById('file').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    const preview = document.getElementById('preview');
+
+    if (file) {
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            preview.src = e.target.result;
+            preview.style.display = 'block';
+        };
+
+        reader.readAsDataURL(file);
+    } else {
+        preview.src = '';
+        preview.style.display = 'none';
+    }
+});
